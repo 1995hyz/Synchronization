e7237bcce194914e035ed7cec7385d28661940ec
1995hyz <1995hyz@gmail.com>
2017-12-21 14:07:48 -0500

    Debug

--- cv.c
+++ cv.c
@@ -8,9 +8,13 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
 void cv_init(struct cv *cv){
-	memset((void *)cv,0,(size_t)(sizeof(char)*4096));
+	cv->totalProcess=0;
+	char *cvaddr=mmap(NULL,(size_t)(sizeof(char)*64),PROT_READ|PROT_WRITE,MAP_SHARED|MAP_ANONYMOUS,-1,0);
+	cv->cvmutex=(struct spinlock *)cvaddr;
+	cv->cvmutex->primeLock=0;
 }
 
 void cv_wait(struct cv *cv, struct spinlock *mutex){
@@ -26,22 +30,24 @@ void cv_wait(struct cv *cv, struct spinlock *mutex){
 	spin_lock(cv->cvmutex);
 	cv->totalProcess++;
 	pid_t currentpid=getpid();
-	cv->pidArray[cv->totalProcess-1]=currentpid;
+	cv->pidArray[cv->totalProcess-1]=(int)currentpid;
+	printf("%d\n",currentpid);
 	spin_unlock(cv->cvmutex);
 	spin_unlock(mutex);
+	printf("$$$$\n");
 	sigsuspend(&sigmask);
 	spin_lock(mutex);
 }
 int cv_broadcast(struct cv *cv){
 	spin_lock(cv->cvmutex);
 	for(cv->totalProcess;cv->totalProcess>=0;cv->totalProcess--){
-		kill(cv->pidArray[cv->totalProcess-1],10);
+		kill((pid_t)cv->pidArray[cv->totalProcess-1],10);
 	}
 	spin_unlock(cv->cvmutex);
 }
 int cv_signal(struct cv *cv){
 	spin_lock(cv->cvmutex);
 	cv->totalProcess--;
-	kill(cv->pidArray[cv->totalProcess],10);
+	kill((pid_t)cv->pidArray[cv->totalProcess],10);
 	spin_unlock(cv->cvmutex);
 }
