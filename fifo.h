6bbd50c7ded092f0a1b799ed86cd457d2a826498
1995hyz <1995hyz@gmail.com>
2017-12-22 23:53:07 -0500

    Add files via upload

---
+++ fifo.h
@@ -0,0 +1,19 @@
+#ifndef FIFO_H
+#define FIFO_H
+
+#define MYFIFO_BUFSIZ 1024
+
+struct fifo{
+	long buf[MYFIFO_BUFSIZ];
+	int write_index;
+	int read_index;
+	struct spinlock *fifo_mutex;
+	struct cv* full;
+	struct cv* empty;
+};
+
+void fifo_init(struct fifo *f);
+void fifo_wr(struct fifo *f,unsigned long d);
+unsigned long fifo_rd(struct fifo *f);
+
+#endif
